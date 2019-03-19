1c1a9daf61fe7e1070e56cfaa4cfba9520bb4f3e
b5156 <304469323@qq.com>
2019-03-19 10:46:46 +0800

    Update wxapp-computed.js

--- wxapp-computed.js
+++ wxapp-computed.js
@@ -44,7 +44,7 @@ function setPath(obj, path, value) {
 // 计算属性装饰器
 function computed(page) {
   if (!isObj(page)) {
-    throw new TypeError("参数必须是一个");
+    throw new TypeError("参数必须是一个Object");
   }
   const _onLoad = page.onLoad;
   page.onLoad = function() {
